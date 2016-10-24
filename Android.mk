bd4eafc8288fa0b0548c0a8dba2bdc2447169725
Mathieu Pasquet <mathieu.pasquet@alterway.fr>
2016-10-24 14:25:39 +0200

    Update makefile

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 IGNORED_WARNINGS := -Wno-sign-compare -Wno-unused-parameter -Wno-sign-promo -Wno-error=return-type
 
-LOCAL_PBUF_INTERMEDIATES := $(call intermediates-dir-for,SHARED_LIBRARIES,libcppsensors_packet,,)/proto/external/aic/libaicd/
+LOCAL_PBUF_INTERMEDIATES := $(call generated-sources-dir-for,SHARED_LIBRARIES,libcppsensors_packet,,)/proto/external/aic/libaicd/
 LOCAL_CFLAGS		:= -DLOG_TAG=\"protobuf_sensors\" -O2 -DGOOGLE_PROTOBUF_NO_RTTI
 LOCAL_SHARED_LIBRARIES := liblog libcutils libstlport libcppsensors_packet
 LOCAL_STATIC_LIBRARIES += libprotobuf-cpp-2.3.0-lite libprotobuf-cpp-2.3.0-full
